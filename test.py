6ffeec5519cc5948aee2a66a97886bb9b03bf3df
zalbo <sbabbari@gmail.com>
2017-10-14 17:07:37 +0200

    inizia a funzionare tkditer

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 import pry
 import re
+import sys
 
 from Tkinter import *
-
-
-
 from tkFileDialog import askopenfilename
+
+
+root = Tk()
+# Create single line text entry box
+x_station_field_text = Entry(root)
+x_station_field_text.pack()
+
+
+
+
+
 
 #open and edit file
 
 def procesfile():
+    x_station = (x_station_field_text.get())
+
 
     file_gcode = askopenfilename(parent=root)
     with open(file_gcode) as f:
@@ -53,30 +64,25 @@ def procesfile():
                     current_x = line_correct_to_insert[1]
                     current_y = line_correct_to_insert[2]
                     current_z = line_correct_to_insert[3]
-                    current_e_precise = line_correct_to_insert[4]
+                    current_e_precise = line_correct_to_insert[4],
+                    pry()
                     content.insert(index + 1 , ";TYPE:CUSTOM\nM83 ;pluginSet extruder to relative mode\nG1 Z10 F300\nG1 X190.000000 Y190.000000 F9000\nG1 Z15 F300\nM84 E0\nG1 Z10 F300\nG1 F9000\nM82\nG1 Z" + current_z + "\nG92 E" + current_e_precise + " ;Set Position\n")
 
 
 #save the file
-        import os
-        filename = os.path.basename(file_gcode).replace('.gcode', '')
-        pry()
-        thefile = open(filename +'_modificato.gcode', 'w+')
-        for item in content:
-            thefile.write(item)
+    import os
+    filename = os.path.basename(file_gcode).replace('.gcode', '')
+    thefile = open(filename +'_modificato.gcode', 'w+')
+    for item in content:
+        thefile.write(item)
 
-root = Tk()
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-Label(root, text="X Station").grid(row=0)
 
-e1 = Entry(root)
-e1.get()
 
-e1.grid(row=0, column=1)
-Button(root, text='EDIT GCODE', command=procesfile).grid(row=3, column=0, sticky=W, pady=4)
+# Print the contents of entry widget to console
 
-root.config(menu=menubar)
+
+# Create a button that will print the contents of the entry
+button = Button(root, text='EDIT GCODE', command=procesfile)
+button.pack()
+
 root.mainloop()
